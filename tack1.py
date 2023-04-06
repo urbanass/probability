87dfbb715447b349ae330555caf9b9eab19502c4
UrbanAss134 <urbanass@bk.ru>
2023-04-06 21:27:15 +0500

    новое

--- tack1.py
+++ tack1.py
@@ -3,9 +3,6 @@ import pandas as pd
 from scipy.stats import  binom
 from scipy.stats import  poisson
 
-def combinations(n, k):
-    return (np.math.factorial(n)/(np.math.factorial(k)*np.math.factorial(n-k)))
-
 #1. Даны значения зарплат из выборки выпускников: 100, 80, 75, 77, 89, 33, 45, 25, 65, 17, 30, 24, 57, 55, 70, 75, 65, 84, 90, 150.
 # Посчитать (желательно без использования статистических методов наподобие std, var, mean) среднее арифметическое,
 # среднее квадратичное отклонение, смещенную и несмещенную оценки дисперсий для данной выборки.
